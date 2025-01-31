90e525efdc4c63f366cb4a8dd2e0596405985257
Henry-Elois <elois.henry@edu.univ-fcomte.fr>
2025-01-31 14:51:43 +0100

    cards images

--- src/backend.mjs
+++ src/backend.mjs
@@ -7,11 +7,17 @@ export async function getOffres() {
       sort: "-created",
     });
 
-    data = data.map((a) => ({...a,img: a.image ? pb.files.getURL(a.image) : null,
-    }));
+    data = data.map((a) => {
+      
+      a.img = pb.files.getURL(a, a.image);
+      return a;
+    });
+
     return data;
-} catch (error) {
-    console.error("Une erreur est survenue en lisant la liste des maisons",error
+  } catch (error) {
+    console.error(
+      "Une erreur est survenue en lisant la liste des maisons",
+      error
     );
     return [];
   }
